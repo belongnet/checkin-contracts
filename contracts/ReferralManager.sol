a3249b43885a38e05304e33578bfde3170891a44
CodeRabbit <contact@coderabbit.ai>
2023-11-26 18:37:09 +0000

    adds an admin role to the ReferralManager contract and restricts the setReferrer function so that only the admin can call it

--- contracts/ReferralManager.sol
+++ contracts/ReferralManager.sol
@@ -14,7 +14,30 @@ contract ReferralManager {
        require(organizer != referrer, "Organizer cannot refer themselves");
         require(referrers[organizer] == address(0), "Organizer already has a referrer");
         referrers[organizer] = referrer;
-        emit ReferrerAssigned(organizer, referrer);
+```diff
+@@ -1,5 +1,11 @@
+ pragma solidity ^0.8.0;
+
++address private admin;
++
++constructor() {
++    admin = msg.sender;
++}
++
+ contract ReferralManager {
+     mapping(address => address) private referrers;
+
+@@ -10,6 +16,11 @@
+     event ReferrerAssigned(address indexed organizer, address indexed referrer);
+
++    modifier onlyAdmin() {
++        require(msg.sender == admin, "Only admin can perform this action");
++        _;
++    }
++
+     function setReferrer(address organizer, address referrer) external onlyAdmin {
+        require(organizer != referrer, "Organizer cannot refer themselves");
+         require(referrers[organizer] == address(0), "Organizer already has a referrer");
     }
 
     function getReferrer(address organizer) external view returns (address) {
