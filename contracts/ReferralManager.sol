9fc6fe9624df4102b6c12c79801e5de963a199f0
jay <jay@toasterclub.com>
2023-11-26 13:42:15 -0500

    fix ReferralManager

--- contracts/ReferralManager.sol
+++ contracts/ReferralManager.sol
@@ -1,53 +1,44 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title ReferralManager
+ * @dev Manages referral relationships between organizers and referrers.
+ */
 contract ReferralManager {
-@@ -14,5 +14,0 @@
--    function getReferrer(address organizer) external view returns (address) {
--        return referrers[organizer];
--    }
--}
+    mapping(address => address) public referrers;
+    address private platformAdmin; // This address represents the platform admin authorized to set referrers.
 
     event ReferrerAssigned(address indexed organizer, address indexed referrer);
 
-    function setReferrer(address organizer, address referrer) external {
-       require(organizer != referrer, "Organizer cannot refer themselves");
-        require(referrers[organizer] == address(0), "Organizer already has a referrer");
-        referrers[organizer] = referrer;
-```diff
-@@ -1,5 +1,11 @@
- pragma solidity ^0.8.0;
-
-+address private admin;
-+
-+constructor() {
-+    admin = msg.sender;
-+}
-+
- contract ReferralManager {
-     mapping(address => address) private referrers;
-
-@@ -10,6 +16,11 @@
-     event ReferrerAssigned(address indexed organizer, address indexed referrer);
-
-+    modifier onlyAdmin() {
-+        require(msg.sender == admin, "Only admin can perform this action");
-+        _;
-+    }
-+
-     function setReferrer(address organizer, address referrer) external onlyAdmin {
-        require(organizer != referrer, "Organizer cannot refer themselves");
-         require(referrers[organizer] == address(0), "Organizer already has a referrer");
+    constructor(address _platformAdmin) {
+        require(_platformAdmin != address(0), "Platform admin address cannot be the zero address");
+        platformAdmin = _platformAdmin;
     }
 
+    modifier onlyPlatformAdmin() {
+        require(msg.sender == platformAdmin, "Only the platform admin can perform this action");
+        _;
+    }
+
+    /**
+     * @dev Sets a referrer for an organizer. Only the platform admin can set a referrer.
+     * @param organizer The address of the organizer.
+     * @param referrer The address of the referrer.
+     */
+    function setReferrer(address organizer, address referrer) external onlyPlatformAdmin {
+        require(organizer != referrer, "Organizer cannot refer themselves");
+        require(referrers[organizer] == address(0), "Organizer already has a referrer");
+        referrers[organizer] = referrer;
+        emit ReferrerAssigned(organizer, referrer);
+    }
+
+    /**
+     * @dev Retrieves the referrer for an organizer.
+     * @param organizer The address of the organizer.
+     * @return The address of the referrer.
+     */
     function getReferrer(address organizer) external view returns (address) {
--    function getReferrer(address organizer) external view returns (address) {
--        return referrers[organizer];
--    }
--}
-@@ -20,5 +20,0 @@
--    function getReferrer(address organizer) external view returns (address) {
--        return referrers[organizer];
--    }
--}
+        return referrers[organizer];
+    }
 }
