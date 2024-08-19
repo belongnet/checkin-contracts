1f3ce0e3e05fcf5b6e0a1d1cd5ba87399e37bbb9
Bo <pukhnobg@gmail.com>
2024-08-19 17:33:14 +0300

    fix: mock

--- contracts/mocks/erc20Example.sol
+++ contracts/mocks/erc20Example.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract erc20Example is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {
+    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, 1000);
     }
 
