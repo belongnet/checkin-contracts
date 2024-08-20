0761efaa84831d0c729ad31e9b2f4a1ca0117bd9
Bo <pukhnobg@gmail.com>
2024-08-20 15:16:38 +0300

    fix: receiver Factory tests

--- test/receiverFactory.test.js
+++ test/receiverFactory.test.js
@@ -1,47 +1,34 @@
-const { ethers } = require("hardhat");
 const { solidity } = require("ethereum-waffle");
-const { BigNumber } = require("ethers");
 const chai = require("chai");
 chai.use(solidity);
 chai.use(require("chai-bignumber")());
 const { expect } = chai;
 
-const BN = require("bn.js");
-const EthCrypto = require("eth-crypto");
-
-const PLATFORM_COMISSION = "10";
-const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
-const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-
-const chainid = 31337;
-
 
 describe('Receiver factory tests', () => {
         let receiverFactory;
 
-        const accounts = waffle.provider.getWallets();
-        const owner = accounts[0];                     
-        const alice = accounts[1];
-        const bob = accounts[2];
-        const charlie = accounts[3];
+        let owner, alice, bob, charlie;
 
         beforeEach(async () => {
+            [owner, alice, bob, charlie] = await ethers.getSigners();
+
             const ReceiverFactory = await ethers.getContractFactory("ReceiverFactory");
 
             receiverFactory = await ReceiverFactory.deploy();
-
         })
 
         it("should correct deploy Receiver instance", async () => {
-            await expect(receiverFactory.deployReceiver([owner.address, alice.address], [3000])).to.be.revertedWith('RoyaltiesReceiver: payees and shares length mismatch');
-            await expect(receiverFactory.deployReceiver([owner.address], [3000])).to.be.revertedWith('RoyaltiesReceiver: there should be only 2 payees');
+            await expect(receiverFactory.deployReceiver([owner.address, alice.address], [3000])).to.be.reverted;
+            await expect(receiverFactory.deployReceiver([owner.address], [3000])).to.be.reverted;
             let tx = await receiverFactory.deployReceiver([owner.address, alice.address], [3000, 7000]);
-            let receipt = await tx.wait()
-            const instanceAddress = receipt.events[2].args.instance;
+            let receipt = await tx.wait();
+
+            const instanceAddress = receipt.events[3].args.instance;
             const receiver = await ethers.getContractAt("RoyaltiesReceiver", instanceAddress);
-            expect(await receiver.payee(0)).to.be.equal(owner.address);
-            expect(await receiver.payee(1)).to.be.equal(alice.address);
-            await expect(receiver.payee(2)).to.be.revertedWith('incorrect index');
+            expect(await receiver.payees(0)).to.be.equal(owner.address);
+            expect(await receiver.payees(1)).to.be.equal(alice.address);
+            await expect(receiver.payees(2)).to.be.reverted;
             expect(await receiver.shares(owner.address)).to.be.equal(3000);
             expect(await receiver.shares(alice.address)).to.be.equal(7000);
             expect(await receiver.totalShares()).to.be.equal(10000);
