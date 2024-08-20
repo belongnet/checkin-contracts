236c68d74488a7f35f566102ab2a3389e64e119e
Bo <pukhnobg@gmail.com>
2024-08-20 15:16:52 +0300

    fix: storage tests

--- test/storage.test.js
+++ test/storage.test.js
@@ -1,31 +1,19 @@
-const { ethers } = require("hardhat");
 const { solidity } = require("ethereum-waffle");
-const { BigNumber } = require("ethers");
 const chai = require("chai");
 chai.use(solidity);
 chai.use(require("chai-bignumber")());
 const { expect } = chai;
 
-const BN = require("bn.js");
-const EthCrypto = require("eth-crypto");
-
-const PLATFORM_COMISSION = "10";
-const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-
-const chainid = 31337;
-
 
 describe('Storage tests', () => {
         let storage;
 
-        const accounts = waffle.provider.getWallets();
-        const owner = accounts[0];                     
-        const alice = accounts[1];
-        const bob = accounts[2];
-        const charlie = accounts[3];
+        let owner, alice, bob, charlie;
 
         beforeEach(async () => {
+            [owner, alice, bob, charlie] = await ethers.getSigners();
+
             const StorageContract = await ethers.getContractFactory("StorageContract");
 
             storage = await StorageContract.deploy();
@@ -33,8 +21,9 @@ describe('Storage tests', () => {
         })
 
         it("check if the contract is empty", async () => {
-            await expect(storage.connect(owner).getInstanceInfo(0)).to.be.revertedWith('incorrect ID');
-            expect(await storage.instancesCount()).to.be.equal(0);
+            await expect(storage.connect(owner).getInstanceInfo(0)).to.be.reverted;
+
+            await expect( storage.instances()).to.be.reverted;
         });
 
         it("shouldn't add instance if not factory", async () => {
@@ -43,13 +32,13 @@ describe('Storage tests', () => {
                 owner.address,
                 "name",
                 "symbol"
-            )).to.be.revertedWith('only factory');
+            )).to.be.reverted;
         });
 
         it("shouldn't set factory if zero", async () => {
             await expect(storage.connect(owner).setFactory(
                 ZERO_ADDRESS
-            )).to.be.revertedWith('incorrect address');
+            )).to.be.reverted;
         });
        
     
