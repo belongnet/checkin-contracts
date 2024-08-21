504bdb63a59fa717129aa1a50d252a35ba562728
Bo <pukhnobg@gmail.com>
2024-08-21 18:14:44 +0300

    add: CreatorToken contract

---
+++ contracts/CreatorToken.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {ITransferValidator721} from "./interfaces/ITransferValidator721.sol";
+
+/// @notice Revert with an error if the transfer validator is being set to the same address.
+error SetTransferValidatorError();
+
+/**
+ * @title  CreatorToken
+ * @notice Functionality to use a transfer validator.
+ */
+abstract contract CreatorToken {
+    event TransferValidatorUpdated(
+        ITransferValidator721 oldValidator,
+        ITransferValidator721 newValidator
+    );
+
+    /// @dev Store the transfer validator. The null address means no transfer validator is set.
+    ITransferValidator721 internal _transferValidator;
+
+    /// @notice Returns the currently active transfer validator.
+    ///         The null address means no transfer validator is set.
+    function getTransferValidator()
+        external
+        view
+        returns (ITransferValidator721)
+    {
+        return _transferValidator;
+    }
+
+    /**
+     * @notice Returns the transfer validation function used.
+     */
+    function getTransferValidationFunction()
+        external
+        pure
+        returns (bytes4 functionSignature, bool isViewFunction)
+    {
+        functionSignature = ITransferValidator721.validateTransfer.selector;
+        isViewFunction = true;
+    }
+
+    /// @notice Set the transfer validator.
+    ///         The external method that uses this must include access control.
+    function _setTransferValidator(
+        ITransferValidator721 newValidator
+    ) internal {
+        ITransferValidator721 oldValidator = _transferValidator;
+
+        if (
+            oldValidator == newValidator &&
+            address(newValidator).code.length == 0
+        ) {
+            revert SetTransferValidatorError();
+        }
+
+        _transferValidator = newValidator;
+
+        _registerTokenType(newValidator);
+
+        emit TransferValidatorUpdated(oldValidator, newValidator);
+    }
+
+    function _validateTansfer(
+        address caller,
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal {
+        // Call the transfer validator if one is set.
+        address transferValidator = address(_transferValidator);
+        if (transferValidator != address(0)) {
+            if (msg.sender == transferValidator) {
+                return;
+            }
+
+            _transferValidator.validateTransfer(caller, from, to, tokenId);
+        }
+    }
+
+    function _registerTokenType(ITransferValidator721 validator) internal {
+        if (address(validator) != address(0)) {
+            uint256 validatorCodeSize;
+            assembly {
+                validatorCodeSize := extcodesize(validator)
+            }
+            if (validatorCodeSize > 0) {
+                try
+                    validator.setTokenTypeOfCollection(
+                        address(this),
+                        uint16(721)
+                    )
+                {} catch {}
+            }
+        }
+    }
+}
