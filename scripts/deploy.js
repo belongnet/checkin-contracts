cfdf54e257e5725b1aafe4e2d8e4d1f770d719cf
Bo <pukhnobg@gmail.com>
2024-08-22 12:56:15 +0300

    fix: Scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,29 @@
-
-const { ethers, upgrades } = require('hardhat');
+const { ethers } = require("hardhat");
 
 async function deploy_nft() {
-  const Factory = await ethers.getContractFactory('Factory');
-  const ReceiverFactory = await ethers.getContractFactory('ReceiverFactory');
-  const Storage = await ethers.getContractFactory('StorageContract');
+  const NFTFactory = await ethers.getContractFactory("NFTFactory");
+  const ReceiverFactory = await ethers.getContractFactory("ReceiverFactory");
+  const Storage = await ethers.getContractFactory("StorageContract");
 
-  console.log('Deploying Storage Contract...');
+  console.log("Deploying Storage Contract...");
   const storage = await Storage.deploy();
-  console.log('Done');
-  
+  console.log("Done");
+
   const signer = "0x284EB52525B5A43b4f590Fc321A6117332EAB414";
   const platformAddress = "0x8eE651E9791e4Fe615796303F48856C1Cf73C885";
-  const platformCommission = "100"; 
+  const platformCommission = "100";
 
-  console.log('Deploying Factory...');
-  const factory = await Factory.deploy();
+  console.log("Deploying NFTFactory...");
+  const factory = await NFTFactory.deploy();
   await factory.deployed();
-  console.log('Done');
-  console.log('Deploying Receiver Factory...');
+  console.log("Done");
+  console.log("Deploying Receiver NFTFactory...");
 
   const receiverFactory = await ReceiverFactory.deploy();
   await receiverFactory.deployed();
 
-  console.log('Done');
-  console.log('Initializing Factory...');
+  console.log("Done");
+  console.log("Initializing NFTFactory...");
 
   await factory.initialize(
     signer,
@@ -33,19 +32,16 @@ async function deploy_nft() {
     storage.address
   );
 
-  console.log('Done');
-  console.log('Initializing Factory...');
-  
+  console.log("Done");
+  console.log("Initializing NFTFactory...");
+
   await storage.deployed();
   await storage.setFactory(factory.address);
-  console.log('Done');
+  console.log("Done");
 
-
-  console.log('Factory deployed to:', factory.address);
-  console.log('Receiver Factory deployed to:', receiverFactory.address);
-  console.log('Storage deployed to:', storage.address);
-
-  
+  console.log("NFTFactory deployed to:", factory.address);
+  console.log("Receiver NFTFactory deployed to:", receiverFactory.address);
+  console.log("Storage deployed to:", storage.address);
 }
 
 deploy_nft();
--- scripts/deployTest.js
+++ scripts/deployTest.js
@@ -1,31 +1,30 @@
-
-const { ethers, upgrades } = require('hardhat');
+const { ethers, upgrades } = require("hardhat");
 
 async function deploy_nft() {
-  const Factory = await ethers.getContractFactory('Factory');
-  const ReceiverFactory = await ethers.getContractFactory('ReceiverFactory');
-  const Storage = await ethers.getContractFactory('StorageContract');
-  const NFT = await ethers.getContractFactory('NFT');
+  const NFTFactory = await ethers.getContractFactory("NFTFactory");
+  const ReceiverFactory = await ethers.getContractFactory("ReceiverFactory");
+  const Storage = await ethers.getContractFactory("StorageContract");
+  const NFT = await ethers.getContractFactory("NFT");
 
-  console.log('Deploying Storage Contract...');
+  console.log("Deploying Storage Contract...");
   const storage = await Storage.deploy();
-  console.log('Done');
-  
+  console.log("Done");
+
   const signer = "0xc204d8492670fC59b946048df140838fdF14D323";
   const platformAddress = "0xc204d8492670fC59b946048df140838fdF14D323";
   const platformCommission = "1";
 
-  console.log('Deploying Factory...');
-  const factory = await Factory.deploy();
+  console.log("Deploying NFTFactory...");
+  const factory = await NFTFactory.deploy();
   await factory.deployed();
-  console.log('Done');
-  console.log('Deploying Receiver Factory...');
+  console.log("Done");
+  console.log("Deploying Receiver NFTFactory...");
 
   const receiverFactory = await ReceiverFactory.deploy();
   await receiverFactory.deployed();
 
-  console.log('Done');
-  console.log('Initializing Factory...');
+  console.log("Done");
+  console.log("Initializing NFTFactory...");
 
   await factory.initialize(
     signer,
@@ -34,12 +33,12 @@ async function deploy_nft() {
     storage.address
   );
 
-  console.log('Done');
-  console.log('Initializing Factory...');
-  
+  console.log("Done");
+  console.log("Initializing NFTFactory...");
+
   await storage.deployed();
   await storage.setFactory(factory.address);
-  console.log('Done');
+  console.log("Done");
 
   // await nft.deployed();
 
@@ -52,12 +51,10 @@ async function deploy_nft() {
   //   "SN"
   // );
 
-  console.log('Factory deployed to:', factory.address);
-  console.log('Receiver Factory deployed to:', receiverFactory.address);
-  console.log('Storage deployed to:', storage.address);
+  console.log("NFTFactory deployed to:", factory.address);
+  console.log("Receiver NFTFactory deployed to:", receiverFactory.address);
+  console.log("Storage deployed to:", storage.address);
   // console.log('NFT deployed to:', nft.address);
-
-  
 }
 
 deploy_nft();
