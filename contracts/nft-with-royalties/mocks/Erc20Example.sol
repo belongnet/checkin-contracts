17455f34940e28dc300eddda38ec415ae7836153
Bo <pukhnobg@gmail.com>
2024-09-17 17:50:39 +0300

    upd: Erc20 mock

--- contracts/nft-with-royalties/mocks/Erc20Example.sol
+++ contracts/nft-with-royalties/mocks/Erc20Example.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract erc20Example is ERC20, Ownable {
+contract Erc20Example is ERC20, Ownable {
     constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {
         _mint(msg.sender, 1000);
     }
