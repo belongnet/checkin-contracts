72331a76962304f3eaa89b04a7ba69ba4c776811
Bo <pukhnobg@gmail.com>
2024-10-01 18:21:40 +0300

    add: natSpecs

--- contracts/factories/NFTFactory.sol
+++ contracts/factories/NFTFactory.sol
@@ -4,19 +4,26 @@ pragma solidity 0.8.27;
 import {Initializable} from "solady/src/utils/Initializable.sol";
 import {Ownable} from "solady/src/auth/Ownable.sol";
 import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
-
 import {ReferralSystem} from "../utils/ReferralSystem.sol";
 import {NFT} from "../NFT.sol";
 import {ReferralPercentages, NftFactoryParameters, NftParameters, InstanceInfo, NftInstanceInfo} from "../Structures.sol";
 
+// ========== Errors ==========
+
 /// @notice Error thrown when the signature provided is invalid.
 error InvalidSignature();
-/// @notice Error thrown when an empty name is provided for an NFT.
+
+/// @notice Error thrown when an empty name or symbol is provided for an NFT.
+/// @param name The name that was passed (empty).
+/// @param symbol The symbol that was passed (empty).
 error EmptyNameSymbolPassed(string name, string symbol);
+
 /// @notice Error thrown when an NFT with the same name and symbol already exists.
 error NFTAlreadyExists();
+
 /// @notice Error thrown when a zero address is passed where it's not allowed.
 error ZeroAddressPassed();
+
 /// @notice Error thrown when an incorrect instance ID is provided.
 error IncorrectInstanceId();
 
@@ -28,7 +35,11 @@ error IncorrectInstanceId();
 contract NFTFactory is Initializable, Ownable, ReferralSystem {
     using SignatureCheckerLib for address;
 
+    // ========== Events ==========
+
     /// @notice Event emitted when a new NFT is created.
+    /// @param _hash The keccak256 hash of the NFT's name and symbol.
+    /// @param info The information about the created NFT instance.
     event NFTCreated(bytes32 _hash, NftInstanceInfo info);
 
     /// @notice Event emitted when the signer address is set.
@@ -55,11 +66,15 @@ contract NFTFactory is Initializable, Ownable, ReferralSystem {
     /// @param arraySize The new maximum array size.
     event MaxArraySizeSet(uint256 arraySize);
 
-    /// @notice A struct that contains _info parameters for the NFTFactory.
+    // ========== State Variables ==========
+
+    /// @notice A struct that contains the NFT factory parameters.
     NftFactoryParameters private _nftFactoryParameters;
 
     /// @notice A mapping from keccak256(name, symbol) to the NFT instance address.
     mapping(bytes32 => NftInstanceInfo) public getNftInstanceInfo;
+
+    // ========== Modifiers ==========
 
     /// @notice Modifier to check if the passed address is not zero.
     /// @param _address The address to check.
@@ -70,15 +85,18 @@ contract NFTFactory is Initializable, Ownable, ReferralSystem {
         _;
     }
 
+    // ========== Functions ==========
+
     /**
-     * @notice Initializes the contract with NFT factory _info and validator.
+     * @notice Initializes the contract with NFT factory parameters and referral percentages.
+     * @param percentages The referral percentages for the system.
+     * @param nftFactoryParameters_ The NFT factory parameters to be set.
      */
     function initialize(
         ReferralPercentages calldata percentages,
         NftFactoryParameters calldata nftFactoryParameters_
     ) external initializer {
         _nftFactoryParameters = nftFactoryParameters_;
-
         _setReferralPercentages(percentages);
         _initializeOwner(msg.sender);
     }
@@ -87,6 +105,7 @@ contract NFTFactory is Initializable, Ownable, ReferralSystem {
      * @notice Produces a new NFT instance.
      * @dev Creates a new instance of the NFT and adds the information to the storage contract.
      * @param _info Struct containing the details of the new NFT instance.
+     * @param referralCode The referral code associated with this NFT instance.
      * @return nftAddress The address of the created NFT instance.
      */
     function produce(
@@ -226,16 +245,17 @@ contract NFTFactory is Initializable, Ownable, ReferralSystem {
         emit TransferValidatorSet(validator);
     }
 
+    /// @notice Sets the referral percentages.
+    /// @dev Can only be called by the owner.
+    /// @param percentages The new referral percentages.
     function setReferralPercentages(
         ReferralPercentages memory percentages
     ) external onlyOwner {
         _setReferralPercentages(percentages);
     }
 
-    /**
-     * @notice Returns the total count of NFT instances stored in the contract.
-     * @return The number of NFT instances.
-     */
+    /// @notice Returns the current NFT factory parameters.
+    /// @return The NFT factory parameters.
     function nftFactoryParameters()
         external
         view
