ebe26d3e5a82c963c41ac04eca73cd5bf002bb04
Bo <pukhnobg@gmail.com>
2024-10-21 17:55:34 +0300

    upd: mocks

--- contracts/mocks/MockTransferValidator.sol
+++ contracts/mocks/MockTransferValidator.sol
@@ -25,4 +25,32 @@ contract MockTransferValidator {
             revert("MockTransferValidator: always reverts");
         }
     }
+
+    function setTokenTypeOfCollection(address, uint16) external {}
+}
+
+contract MockTransferValidatorV2 {
+    bool internal switcher;
+
+    constructor(bool _switcher) {
+        switcher = _switcher;
+    }
+
+    function setSwitcher(bool _switcher) external {
+        switcher = _switcher;
+    }
+
+    function validateTransfer(
+        address,
+        /* caller */
+        address,
+        /* from */
+        address,
+        /* to */
+        uint256 /* tokenId */
+    ) external view {
+        if (!switcher) {
+            revert("MockTransferValidator: always reverts");
+        }
+    }
 }
