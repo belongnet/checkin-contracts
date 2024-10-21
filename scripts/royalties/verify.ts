6ff1cbf5bd21d121f0005655773a66cc655805b4
Bo <pukhnobg@gmail.com>
2024-10-21 17:56:08 +0300

    upd: scripts

--- scripts/royalties/verify.ts
+++ scripts/royalties/verify.ts
@@ -7,68 +7,68 @@ const NFTFactory_Address = "0x100FEb2D822CBb32C4e8f047D43615AC8851Ed79"; //"0x4F
 const ReceiverFactory_Address = "0x9e3743dEC51b82BD83d7fF7557650BF1C75ee096"; //"0xfb2668b47f93b168ef99EA95d28bd31dB723ad79";
 const TransferValidator_Address = "0x935AaAE808d09C2BDf6840bB85dB9eC7c82fBA7c"; //"0xDD001eb79ce6aa03d79C3C510CFb8CB16C89d8A7";
 
-const receiver_address = "0x01ea4029de725502907de311d84324572641a794";
-const nft_address = '0x5f8e6669a0cd06b9797b4ea285f9fc6b5cb58115';
+const receiver_address = "0xeee1ee6a66c4b27f4dd841d79356eb124e243ff4";
+const nft_address = '0xc1725524e6a47473259ac0b0efe8fad7e74264ee';
 
 async function verify() {
   console.log("Verification: ");
 
-  try {
-    verifyContract(NFTFactory_Address);
-    console.log("NFTFactory verification successful.");
-  } catch (error) {
-    console.error("NFTFactory verification failed:", error);
-  }
-
-  try {
-    verifyContract(ReceiverFactory_Address);
-    console.log("ReceiverFactory verification successful.");
-  } catch (error) {
-    console.error("ReceiverFactory verification failed:", error);
-  }
-
-  try {
-    verifyContract(TransferValidator_Address, [true]);
-    console.log("ReceiverFactory verification successful.");
-  } catch (error) {
-    console.error("ReceiverFactory verification failed:", error);
-  }
-
-  // const info: InstanceInfoStruct = {
-  //   name: "Blast New Smart",
-  //   symbol: "blast-new-smart",
-  //   contractURI: "https://foster-images.s3.us-east-1.amazonaws.com/up/assets/nft/blast-new-smart/blast-new-smart.json",
-  //   payingToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-  //   mintPrice: 10000000000000000n,
-  //   whitelistMintPrice: 10000000000000000n,
-  //   transferable: true,
-  //   maxTotalSupply: 9999,
-  //   feeNumerator: 1000,
-  //   feeReceiver: receiver_address,
-  //   collectionExpire: 1730330820,
-  //   signature: '0x8a77ebcde14ae25b6eeb640139ef1865e7d8b8be95d2b1b7d7e9d1f427da22a21fd852ff63ff6c20260ba5393c92c7e2905696fab0ffe972a64b74b8541d7a861b'
-  // }
-
-  // const params: NftParametersStruct = {
-  //   transferValidator: TransferValidator_Address,
-  //   factory: NFTFactory_Address,
-  //   info,
-  //   creator: '0x192De36d0A4a23FE101a38a3722557113a8e7F77',
-  //   referralCode: ethers.constants.HashZero,
+  // try {
+  //   await verifyContract(NFTFactory_Address);
+  //   console.log("NFTFactory verification successful.");
+  // } catch (error) {
+  //   console.error("NFTFactory verification failed:", error);
   // }
 
   // try {
-  //   verifyContract(nft_address, [params]);
-  //   console.log("NFT verification successful.");
+  //   await verifyContract(ReceiverFactory_Address);
+  //   console.log("ReceiverFactory verification successful.");
   // } catch (error) {
-  //   console.error("NFT verification failed:", error);
+  //   console.error("ReceiverFactory verification failed:", error);
   // }
+
+  // try {
+  //   await verifyContract(TransferValidator_Address, [true]);
+  //   console.log("ReceiverFactory verification successful.");
+  // } catch (error) {
+  //   console.error("ReceiverFactory verification failed:", error);
+  // }
+
+  const info: InstanceInfoStruct = {
+    name: "Event Amoy USDC Nomad Token",
+    symbol: "	event-amoy-usdc-nomad-token",
+    contractURI: "https://foster-images.s3.us-east-1.amazonaws.com/up/assets/nft/event-amoy-usdc-nomad-token/event-amoy-usdc-nomad-token.json",
+    payingToken: '0x14196F08a4Fa0B66B7331bC40dd6bCd8A1dEeA9F',
+    mintPrice: 100000n,
+    whitelistMintPrice: 1000n,
+    transferable: true,
+    maxTotalSupply: 300,
+    feeNumerator: 1000,
+    feeReceiver: receiver_address,
+    collectionExpire: 1732215780,
+    signature: '0xfdcf6642bcc6ed43e68f76ea9d78f1000a94ef4f05770fa3807aed04962468da7e0e77b3432c0e1cc605b9c36e4bb244209b9ad1f7d3099a769bfc4cac42f1641b'
+  }
+
+  const params: NftParametersStruct = {
+    transferValidator: TransferValidator_Address,
+    factory: NFTFactory_Address,
+    info,
+    creator: '0x192De36d0A4a23FE101a38a3722557113a8e7F77',
+    referralCode: ethers.constants.HashZero,
+  }
+
+  try {
+    await verifyContract(nft_address, [params]);
+    console.log("NFT verification successful.");
+  } catch (error) {
+    console.error("NFT verification failed:", error);
+  }
 
   // const payees = ['0x8eE651E9791e4Fe615796303F48856C1Cf73C885', '0x192De36d0A4a23FE101a38a3722557113a8e7F77'];
   // const shares = [2000, 8000];
 
   // try {
-  //   verifyContract(receiver_address, [payees, shares]);
+  //   await verifyContract(receiver_address, [payees, shares]);
   //   console.log("Receiver verification successful.");
   // } catch (error) {
   //   console.error("Receiververification failed:", error);
