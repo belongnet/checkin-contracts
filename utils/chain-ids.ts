96434a9fbfe1e8a9ad948bbcb2842ba334d83e0b
Bo <pukhnobg@gmail.com>
2025-01-31 15:20:15 +0200

    fix: config

---
+++ utils/chain-ids.ts
@@ -0,0 +1,95 @@
+export enum ChainIds {
+	mainnet = 1,
+	bsc = 56,
+	polygon = 137,
+	blast = 81457,
+	celo = 42220,
+	base = 8453,
+	linea = 59144,
+	astar = 592,
+	skale_europa = 2046399126,
+	skale_nebula = 1482601649,
+	skale_calypso = 1564830818,
+	sepolia = 11155111,
+	blast_sepolia = 168587773,
+	skale_calypso_testnet = 974399131,
+	amoy = 80002,
+}
+
+export const chainRPCs = (chainid: ChainIds, apiKey?: string): string => {
+	if (chainid === ChainIds.mainnet || chainid === ChainIds.polygon || chainid == ChainIds.sepolia) {
+		if (apiKey == undefined || apiKey == '' || apiKey == null) {
+			throw Error('Proive api for the network.');
+		}
+	}
+
+	switch (chainid) {
+		case ChainIds.mainnet:
+			return `https://mainnet.infura.io/v3/${apiKey}`;
+		case ChainIds.bsc:
+			return "https://bsc-dataseed.binance.org";
+		case ChainIds.polygon:
+			return `https://polygon-mainnet.infura.io/v3/${apiKey}`;
+		case ChainIds.blast:
+			return `https://rpc.envelop.is/blast`;
+		case ChainIds.celo:
+			return `https://rpc.ankr.com/celo`;
+		case ChainIds.base:
+			return `https://base.llamarpc.com`;
+		case ChainIds.linea:
+			return `https://linea-rpc.publicnode.com`;
+		case ChainIds.astar:
+			return `https://1rpc.io/astr`;
+		case ChainIds.skale_europa:
+			return `https://mainnet.skalenodes.com/v1/elated-tan-skat`;
+		case ChainIds.skale_nebula:
+			return `https://mainnet.skalenodes.com/v1/green-giddy-denebola`;
+		case ChainIds.skale_calypso:
+			return `https://mainnet.skalenodes.com/v1/honorable-steel-rasalhague`;
+		case ChainIds.sepolia:
+			return `https://sepolia.infura.io/v3/${apiKey}`;
+		case ChainIds.amoy:
+			return `https://rpc-amoy.polygon.technology`;
+		case ChainIds.blast_sepolia:
+			return `https://sepolia.blast.io`;
+		case ChainIds.skale_calypso_testnet:
+			return "https://testnet.skalenodes.com/v1/giant-half-dual-testnet";
+		default:
+			throw Error('No networks provided');
+	}
+}
+
+export const blockscanUrls = (chainid: ChainIds, apiKey?: string): string => {
+	if (chainid === ChainIds.mainnet || chainid === ChainIds.polygon || chainid == ChainIds.sepolia) {
+		if (apiKey == undefined || apiKey == '' || apiKey == null) {
+			throw Error('Proive api for the network.');
+		}
+	}
+
+	switch (chainid) {
+		case ChainIds.blast:
+			return `https://blastscan.io/`;
+		case ChainIds.celo:
+			return `https://celoscan.io/`;
+		case ChainIds.base:
+			return `https://basescan.org/`;
+		case ChainIds.linea:
+			return `https://lineascan.build/`;
+		case ChainIds.astar:
+			return `https://astar.blockscout.com/`;
+		case ChainIds.blast_sepolia:
+			return `https://sepolia.blastscan.io/`;
+		case ChainIds.amoy:
+			return `https://amoy.polygonscan.com`;
+		case ChainIds.skale_europa:
+			return `https://elated-tan-skat.explorer.mainnet.skalenodes.com/`;
+		case ChainIds.skale_nebula:
+			return `https://green-giddy-denebola.explorer.mainnet.skalenodes.com/`;
+		case ChainIds.skale_calypso:
+			return `https://honorable-steel-rasalhague.explorer.mainnet.skalenodes.com/`;
+		case ChainIds.skale_calypso_testnet:
+			return `https://giant-half-dual-testnet.explorer.testnet.skalenodes.com/`;
+		default:
+			throw Error('No networks provided');
+	}
+}
