c6821722871227da2706c9cc9bc49ed2d2f47568
Bohdan Pukhno <pukhnobg@gmail.com>
2025-06-17 23:17:18 +0300

    add: v2 of contracts

---
+++ contracts/RoyaltiesReceiverV2.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {Initializable} from "solady/src/utils/Initializable.sol";
+import {SafeTransferLib} from "solady/src/utils/SafeTransferLib.sol";
+
+import {NFTFactoryV2} from "./factories/NFTFactoryV2.sol";
+import {Releases} from "./StructuresV2.sol";
+
+/// @notice Thrown when an account is not due for payment.
+error AccountNotDuePayment(address account);
+
+/// @notice Thrown when transfer is not to a payee.
+error OnlyToPayee();
+
+/**
+ * @title RoyaltiesReceiver
+ * @notice A contract for managing and releasing royalty payments in both native Ether and ERC20 tokens.
+ * @dev Handles payment distribution based on shares assigned to payees. Fork of OZ's PaymentSplitter with some changes.
+ * The only change is that common `release()` functions are replaced with `releaseAll()` functions,
+ * which allow the caller to transfer funds for both the creator and the platform.
+ */
+contract RoyaltiesReceiverV2 is Initializable {
+    using SafeTransferLib for address;
+
+    /// @notice Emitted when a new payee is added to the contract.
+    /// @param account The address of the new payee.
+    /// @param shares The number of shares assigned to the payee.
+    event PayeeAdded(address indexed account, uint256 shares);
+
+    /// @notice Emitted when a payment in native Ether is released.
+    /// @param token The address of the ERC20 token if address(0) then native currency.
+    /// @param to The address receiving the payment.
+    /// @param amount The amount of Ether released.
+    event PaymentReleased(
+        address indexed token,
+        address indexed to,
+        uint256 amount
+    );
+
+    /// @notice Emitted when the contract receives native Ether.
+    /// @param from The address sending the Ether.
+    /// @param amount The amount of Ether received.
+    event PaymentReceived(address indexed from, uint256 amount);
+
+    /**
+     * @title RoyaltiesReceivers
+     * @notice Payee addresses for royalty splits
+     * @dev Used by RoyaltiesReceiver to distribute payments
+     */
+    struct RoyaltiesReceivers {
+        /// @notice Address receiving creator share
+        address creator;
+        /// @notice Address receiving platform share
+        address platform;
+        /// @notice Address receiving referral share (optional)
+        address referral;
+    }
+
+    /// @notice The constant address representing ETH.
+    address public constant ETH_ADDRESS =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /// @notice Total shares amount.
+    uint256 public constant TOTAL_SHARES = 10000;
+
+    address public nftFactory;
+
+    bytes32 public referralCode;
+
+    /**
+     * @notice List of payee addresses. Returns the address of the payee at the given index.
+     */
+    RoyaltiesReceivers public payees;
+
+    /// @notice Struct for tracking native Ether releases.
+    Releases private nativeReleases;
+
+    /// @notice Mapping of ERC20 token addresses to their respective release tracking structs.
+    mapping(address => Releases) private erc20Releases;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    // constructor() {
+    //     _disableInitializers();
+    // }
+
+    /**
+     * @notice Initializes the contract with a list of payees and their respective shares.
+     * @param payees_ The list of payee addresses.
+     */
+    function initialize(
+        RoyaltiesReceivers calldata payees_,
+        address _nftFactory,
+        bytes32 referralCode_
+    ) external initializer {
+        nftFactory = _nftFactory;
+        payees = payees_;
+        referralCode = referralCode_;
+    }
+
+    function shares(address account) public view returns (uint256) {
+        NFTFactoryV2 factory = NFTFactoryV2(nftFactory);
+        if (account == payees.creator) {
+            return factory.royaltiesParameters().amountToCreator;
+        } else {
+            uint256 platformShare = factory
+                .royaltiesParameters()
+                .amountToPlatform;
+            uint256 referralShare = factory.getReferralShare(
+                payees.creator,
+                referralCode
+            );
+            if (referralShare > 0 && payees.referral != address(0)) {
+                unchecked {
+                    platformShare -= referralShare;
+                }
+            }
+
+            if (account == payees.platform) {
+                return platformShare;
+            } else if (account != address(0) && account == payees.referral) {
+                return referralShare;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    /**
+     * @notice Logs the receipt of Ether. Called when the contract receives Ether.
+     */
+    receive() external payable {
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Releases all pending payments for a given currency to the payees.
+     * @param token The address of the currecny to be released (ERC20 token address or address(0) for native Ether).
+     */
+    function releaseAll(address token) external {
+        RoyaltiesReceivers memory payees_ = payees;
+
+        _release(token, payees_.creator);
+
+        _release(token, payees_.platform);
+        if (payees_.referral != address(0)) {
+            _release(token, payees_.referral);
+        }
+    }
+
+    /**
+     * @notice Releases pending ERC20 token payments for a given token to the payee.
+     * @param token The address of the currecny to be released (ERC20 token address or address(0) for native Ether).
+     */
+    function release(address token, address to) external {
+        _onlyToPayee(to);
+
+        _release(token, to);
+    }
+
+    /**
+     * @notice Returns the total amount of a specific currency already released to payees.
+     * @param token The address of the currecny to be released (ERC20 token address or address(0) for native Ether).
+     * @return The total amount released.
+     */
+    function totalReleased(address token) external view returns (uint256) {
+        if (token == ETH_ADDRESS) {
+            return nativeReleases.totalReleased;
+        } else {
+            return erc20Releases[token].totalReleased;
+        }
+    }
+
+    /**
+     * @notice Returns the amount of a specific ERC20 token already released to a specific payee.
+     * @param token The address of the ERC20 token.
+     * @param account The address of the payee.
+     * @return The amount of tokens released to the payee.
+     */
+    function released(
+        address token,
+        address account
+    ) external view returns (uint256) {
+        if (token == ETH_ADDRESS) {
+            return nativeReleases.released[account];
+        } else {
+            return erc20Releases[token].released[account];
+        }
+    }
+
+    /**
+     * @dev Internal function to release the pending payment for a payee.
+     * @param token The ERC20 token address, or address(0) for native Ether.
+     * @param account The payee's address receiving the payment.
+     */
+    function _release(address token, address account) internal {
+        uint256 payment = _pendingPayment(token, account);
+
+        if (payment == 0) {
+            return;
+        }
+
+        if (token == ETH_ADDRESS) {
+            nativeReleases.released[account] += payment;
+            nativeReleases.totalReleased += payment;
+
+            account.safeTransferETH(payment);
+        } else {
+            erc20Releases[token].released[account] += payment;
+            erc20Releases[token].totalReleased += payment;
+
+            token.safeTransfer(account, payment);
+        }
+
+        emit PaymentReleased(token, account, payment);
+    }
+
+    /**
+     * @dev Internal logic for computing the pending payment for an account.
+     * @param account The address of the payee.
+     * @return The amount of funds still owed to the payee.
+     */
+    function _pendingPayment(
+        address token,
+        address account
+    ) private view returns (uint256) {
+        bool isNativeRelease = token == ETH_ADDRESS;
+
+        uint256 totalReceived = isNativeRelease
+            ? address(this).balance + nativeReleases.totalReleased
+            : token.balanceOf(address(this)) +
+                erc20Releases[token].totalReleased;
+        uint256 alreadyReleased = isNativeRelease
+            ? nativeReleases.released[account]
+            : erc20Releases[token].released[account];
+
+        uint256 payment = (totalReceived * shares(account)) / TOTAL_SHARES;
+
+        if (payment <= alreadyReleased) {
+            return 0;
+        }
+
+        return payment - alreadyReleased;
+    }
+
+    function _onlyToPayee(address account) private view {
+        RoyaltiesReceivers memory payees_ = payees;
+
+        require(
+            payees_.creator == account ||
+                payees_.platform == account ||
+                (payees_.referral != address(0) && payees_.referral == account),
+            AccountNotDuePayment(account)
+        );
+    }
+}
---
+++ contracts/factories/NFTFactoryV2.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+
+import {Initializable} from "solady/src/utils/Initializable.sol";
+import {Ownable} from "solady/src/auth/Ownable.sol";
+import {SignatureCheckerLib} from "solady/src/utils/SignatureCheckerLib.sol";
+import {LibClone} from "solady/src/utils/LibClone.sol";
+
+import {ReferralSystemV2} from "./utils/ReferralSystemV2.sol";
+
+import {NFTV2} from "../NFTV2.sol";
+
+import {RoyaltiesReceiverV2} from "../RoyaltiesReceiverV2.sol";
+
+import {NftFactoryParameters, NftParameters, InstanceInfo, NftInstanceInfo, InvalidSignature} from "../StructuresV2.sol";
+
+// ========== Errors ==========
+
+/// @notice Error thrown when an NFT with the same name and symbol already exists.
+error NFTAlreadyExists();
+
+/**
+ * @title NFT Factory Contract
+ * @notice A factory contract to create new NFT instances with specific parameters.
+ * @dev This contract allows producing NFTs, managing platform settings, and verifying signatures.
+ */
+contract NFTFactoryV2 is Initializable, Ownable, ReferralSystemV2 {
+    using SignatureCheckerLib for address;
+    using LibClone for address;
+
+    // ========== Events ==========
+
+    /// @notice Event emitted when a new NFT is created.
+    /// @param _hash The keccak256 hash of the NFT's name and symbol.
+    /// @param info The information about the created NFT instance.
+    event NFTCreated(bytes32 indexed _hash, NftInstanceInfo info);
+
+    /// @notice Event emitted when the new factory parameters set.
+    /// @param nftFactoryParameters The NFT factory parameters to be set.
+    /// @param percentages The referral percentages for the system.
+    event FactoryParametersSet(
+        NftFactoryParameters nftFactoryParameters,
+        uint16[5] percentages
+    );
+    /// @notice Emitted when the implementation address is upgraded.
+    event ImplementationUpgraded(Implementations currentImplementations);
+    event RoyaltiesUpgraded(RoyaltiesParameters amountToCreator);
+
+    // ========== State Variables ==========
+
+    /// @notice A struct that contains the NFT factory parameters.
+    NftFactoryParameters private _nftFactoryParameters;
+
+    /// @notice A mapping from keccak256(name, symbol) to the NFT instance address.
+    mapping(bytes32 => NftInstanceInfo) public getNftInstanceInfo;
+
+    struct RoyaltiesParameters {
+        uint16 amountToCreator;
+        uint16 amountToPlatform;
+    }
+
+    struct Implementations {
+        address nft;
+        address royaltiesReceiver;
+    }
+
+    Implementations public currentImplementations;
+
+    RoyaltiesParameters internal _royaltiesParameters;
+
+    error TotalRoyaltiesExceed100Pecents();
+
+    // ========== Functions ==========
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @notice Initializes the contract with NFT factory parameters and referral percentages.
+     * @param nftFactoryParameters_ The NFT factory parameters to be set.
+     * @param percentages The referral percentages for the system.
+     */
+    function initialize(
+        NftFactoryParameters calldata nftFactoryParameters_,
+        uint16[5] calldata percentages,
+        RoyaltiesParameters calldata _royalties,
+        Implementations calldata _implementations
+    ) external initializer {
+        _nftFactoryParameters = nftFactoryParameters_;
+        _setReferralPercentages(percentages);
+
+        _initializeOwner(msg.sender);
+
+        _upgradeRoyalties(_royalties);
+        _upgradeImplementations(_implementations);
+    }
+
+    // function upgradeToV2(
+    //     RoyaltiesParameters calldata _royalties,
+    //     Implementations calldata _implementations
+    // ) external reinitializer(2) {
+    //     _upgradeRoyalties(_royalties);
+    //     _upgradeImplementations(_implementations);
+    // }
+
+    /// @notice Upgrades the implementation contract for future clones.
+    /// @dev Only callable by the contract owner.
+    function upgradeImplementations(
+        Implementations calldata _implementations
+    ) external onlyOwner {
+        _upgradeImplementations(_implementations);
+    }
+
+    /// @notice Upgrades the implementation contract for future clones.
+    /// @dev Only callable by the contract owner.
+    function upgradeRoyalties(
+        RoyaltiesParameters calldata _royalties
+    ) external onlyOwner {
+        _upgradeRoyalties(_royalties);
+    }
+
+    /**
+     * @notice Produces a new NFT i nstance.
+     * @dev Creates a new instance of the NFT and adds the information to the storage contract.
+     * @param _info Struct containing the details of the new NFT instance.
+     * @param referralCode The referral code associated with this NFT instance.
+     * @return nftAddress The address of the created NFT instance.
+     */
+    function produce(
+        InstanceInfo memory _info,
+        bytes32 referralCode
+    ) external returns (address nftAddress) {
+        NftFactoryParameters memory params = _nftFactoryParameters;
+
+        // Name, symbol signed through BE, and checks if the size > 0.
+        if (
+            !params.signerAddress.isValidSignatureNow(
+                keccak256(
+                    abi.encodePacked(
+                        _info.metadata.name,
+                        _info.metadata.symbol,
+                        _info.contractURI,
+                        _info.feeNumerator,
+                        block.chainid
+                    )
+                ),
+                _info.signature
+            )
+        ) {
+            revert InvalidSignature();
+        }
+
+        bytes32 salt = keccak256(
+            abi.encodePacked(_info.metadata.name, _info.metadata.symbol)
+        );
+
+        require(
+            getNftInstanceInfo[salt].nftAddress == address(0),
+            NFTAlreadyExists()
+        );
+
+        _info.paymentToken = _info.paymentToken == address(0)
+            ? params.defaultPaymentCurrency
+            : _info.paymentToken;
+
+        address receiver;
+
+        _setReferralUser(referralCode, msg.sender);
+        if (_info.feeNumerator > 0) {
+            receiver = currentImplementations.royaltiesReceiver.clone();
+            RoyaltiesReceiverV2(payable(receiver)).initialize(
+                RoyaltiesReceiverV2.RoyaltiesReceivers(
+                    msg.sender,
+                    params.platformAddress,
+                    referrals[referralCode].creator
+                ),
+                address(this),
+                referralCode
+            );
+        }
+
+        nftAddress = currentImplementations.nft.cloneDeterministic(salt);
+        NFTV2(nftAddress).initialize(
+            NftParameters({
+                transferValidator: params.transferValidator,
+                factory: address(this),
+                info: _info,
+                creator: msg.sender,
+                feeReceiver: receiver,
+                referralCode: referralCode
+            })
+        );
+
+        NftInstanceInfo memory info = NftInstanceInfo({
+            creator: msg.sender,
+            nftAddress: nftAddress,
+            metadata: _info.metadata,
+            royaltiesReceiver: receiver
+        });
+
+        getNftInstanceInfo[salt] = info;
+
+        emit NFTCreated(salt, info);
+    }
+
+    /**
+     * @notice Sets new factory parameters.
+     * @dev Can only be called by the owner (BE).
+     * @param nftFactoryParameters_ The NFT factory parameters to be set.
+     * @param percentages An array containing the referral percentages for initial, second, third, and default use.
+     */
+    function setFactoryParameters(
+        NftFactoryParameters calldata nftFactoryParameters_,
+        uint16[5] calldata percentages
+    ) external onlyOwner {
+        _nftFactoryParameters = nftFactoryParameters_;
+        _setReferralPercentages(percentages);
+
+        emit FactoryParametersSet(nftFactoryParameters_, percentages);
+    }
+
+    /// @notice Returns the current NFT factory parameters.
+    /// @return The NFT factory parameters.
+    function nftFactoryParameters()
+        external
+        view
+        returns (NftFactoryParameters memory)
+    {
+        return _nftFactoryParameters;
+    }
+
+    function royaltiesParameters()
+        external
+        view
+        returns (RoyaltiesParameters memory)
+    {
+        return _royaltiesParameters;
+    }
+
+    function getReferralShare(
+        address referralUser,
+        bytes32 code
+    ) public view returns (uint256) {
+        return
+            getReferralRate(
+                referralUser,
+                code,
+                _royaltiesParameters.amountToPlatform
+            );
+    }
+
+    function _upgradeImplementations(
+        Implementations calldata _implementations
+    ) private {
+        currentImplementations = _implementations;
+        emit ImplementationUpgraded(_implementations);
+    }
+
+    function _upgradeRoyalties(
+        RoyaltiesParameters calldata _royalties
+    ) private {
+        require(
+            _royalties.amountToCreator + _royalties.amountToPlatform <= 10000,
+            TotalRoyaltiesExceed100Pecents()
+        );
+
+        _royaltiesParameters = _royalties;
+
+        emit RoyaltiesUpgraded(_royalties);
+    }
+}
