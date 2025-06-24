adf61f8379a1a705ac179aef101a7f432cbcd795
Bohdan Pukhno <pukhnobg@gmail.com>
2025-06-24 18:11:07 +0300

    add: LONG

---
+++ contracts/v2/tokens/LONG.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity 0.8.27;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Bridgeable} from "@openzeppelin/community-contracts/token/ERC20/extensions/ERC20Bridgeable.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract LONG is
+    ERC20,
+    ERC20Bridgeable,
+    ERC20Burnable,
+    ERC20Pausable,
+    AccessControl,
+    ERC20Permit
+{
+    address internal constant SUPERCHAIN_TOKEN_BRIDGE =
+        0x4200000000000000000000000000000000000028;
+    error Unauthorized();
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant BACKEND_ROLE = keccak256("BACKEND_ROLE");
+
+    constructor(
+        address defaultAdmin,
+        address pauser,
+        address minter,
+        address burner,
+        address backend
+    ) ERC20("LONG", "LONG") ERC20Permit("LONG") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(PAUSER_ROLE, pauser);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(BURNER_ROLE, burner);
+        _grantRole(BACKEND_ROLE, BACKEND_ROLE);
+    }
+
+    /**
+     * @dev Checks if the caller is the predeployed SuperchainTokenBridge. Reverts otherwise.
+     *
+     * IMPORTANT: The predeployed SuperchainTokenBridge is only available on chains in the Superchain.
+     */
+    function _checkTokenBridge(address caller) internal pure override {
+        if (caller != SUPERCHAIN_TOKEN_BRIDGE) revert Unauthorized();
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC20Bridgeable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
