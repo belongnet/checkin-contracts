ec55a2ac18bfffbd7e495a1b72b9250c3aa8cb10
Bohdan Pukhno <pukhnobg@gmail.com>
2025-07-28 18:35:08 +0300

    add: price feed interface

---
+++ contracts/v2/interfaces/ILONGPriceFeed.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {AggregatorV2V3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV2V3Interface.sol";
+
+interface ILONGPriceFeed is AggregatorV2V3Interface {}
