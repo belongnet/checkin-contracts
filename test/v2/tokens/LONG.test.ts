0778d467bd9eef931615df9a8ae1541c413c8a78
Bohdan Pukhno <pukhnobg@gmail.com>
2025-08-04 15:03:30 +0300

    test: updated LONG tests

--- test/v2/tokens/LONG.test.ts
+++ test/v2/tokens/LONG.test.ts
@@ -1,94 +1,92 @@
 import { ethers } from 'hardhat';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import { ContractFactory } from 'ethers';
 import { expect } from 'chai';
 import { LONG } from '../../../typechain-types';
+import { deployLONG } from '../helpers/deployFixtures';
 
 describe('LONG', () => {
   async function fixture() {
     const [admin, pauser, minter, burner] = await ethers.getSigners();
 
-    const LONG: ContractFactory = await ethers.getContractFactory('LONG');
-    const long: LONG = (await LONG.deploy(admin.address, pauser.address, minter.address, burner.address)) as LONG;
-    await long.deployed();
+    const LONG: LONG = await deployLONG(admin.address, pauser.address, minter.address, burner.address);
 
     return {
       admin,
       pauser,
       minter,
       burner,
-      long,
+      LONG,
     };
   }
 
   describe('Deployment', () => {
     it('Should be deployed correctly', async () => {
-      const { long, admin, pauser, minter, burner } = await loadFixture(fixture);
+      const { LONG, admin, pauser, minter, burner } = await loadFixture(fixture);
 
-      expect(long.address).to.be.properAddress;
+      expect(LONG.address).to.be.properAddress;
 
-      expect(await long.name()).to.eq('LONG');
-      expect(await long.symbol()).to.eq('LONG');
-      expect((await long.eip712Domain()).name).to.eq('LONG');
+      expect(await LONG.name()).to.eq('LONG');
+      expect(await LONG.symbol()).to.eq('LONG');
+      expect((await LONG.eip712Domain()).name).to.eq('LONG');
 
-      expect(await long.hasRole(await long.DEFAULT_ADMIN_ROLE(), admin.address)).to.be.true;
-      expect(await long.hasRole(await long.PAUSER_ROLE(), pauser.address)).to.be.true;
-      expect(await long.hasRole(await long.MINTER_ROLE(), minter.address)).to.be.true;
-      expect(await long.hasRole(await long.BURNER_ROLE(), burner.address)).to.be.true;
+      expect(await LONG.hasRole(await LONG.DEFAULT_ADMIN_ROLE(), admin.address)).to.be.true;
+      expect(await LONG.hasRole(await LONG.PAUSER_ROLE(), pauser.address)).to.be.true;
+      expect(await LONG.hasRole(await LONG.MINTER_ROLE(), minter.address)).to.be.true;
+      expect(await LONG.hasRole(await LONG.BURNER_ROLE(), burner.address)).to.be.true;
     });
   });
 
   describe('Mint Burn Pause', () => {
     it('mint() only with MINTER_ROLE', async () => {
-      const { long, admin, minter } = await loadFixture(fixture);
+      const { LONG, admin, minter } = await loadFixture(fixture);
 
-      await expect(long.connect(admin).mint(admin.address, 1000))
-        .to.be.revertedWithCustomError(long, 'AccessControlUnauthorizedAccount')
-        .withArgs(admin.address, await long.MINTER_ROLE());
+      await expect(LONG.connect(admin).mint(admin.address, 1000))
+        .to.be.revertedWithCustomError(LONG, 'AccessControlUnauthorizedAccount')
+        .withArgs(admin.address, await LONG.MINTER_ROLE());
 
-      const tx = await long.connect(minter).mint(admin.address, 1000);
+      const tx = await LONG.connect(minter).mint(admin.address, 1000);
 
-      await expect(tx).to.emit(long, 'Transfer').withArgs(ethers.constants.AddressZero, admin.address, 1000);
+      await expect(tx).to.emit(LONG, 'Transfer').withArgs(ethers.constants.AddressZero, admin.address, 1000);
     });
 
     it('burn() only with BURNER_ROLE', async () => {
-      const { long, admin, minter, burner } = await loadFixture(fixture);
+      const { LONG, admin, minter, burner } = await loadFixture(fixture);
 
-      await long.connect(minter).mint(admin.address, 1000);
+      await LONG.connect(minter).mint(admin.address, 1000);
 
-      await expect(long.connect(admin)['burn(address,uint256)'](admin.address, 1000))
-        .to.be.revertedWithCustomError(long, 'AccessControlUnauthorizedAccount')
-        .withArgs(admin.address, await long.BURNER_ROLE());
+      await expect(LONG.connect(admin)['burn(address,uint256)'](admin.address, 1000))
+        .to.be.revertedWithCustomError(LONG, 'AccessControlUnauthorizedAccount')
+        .withArgs(admin.address, await LONG.BURNER_ROLE());
 
-      const tx = await long.connect(burner)['burn(address,uint256)'](admin.address, 1000);
+      const tx = await LONG.connect(burner)['burn(address,uint256)'](admin.address, 1000);
 
-      await expect(tx).to.emit(long, 'Transfer').withArgs(admin.address, ethers.constants.AddressZero, 1000);
+      await expect(tx).to.emit(LONG, 'Transfer').withArgs(admin.address, ethers.constants.AddressZero, 1000);
     });
 
     it('pause() only with PAUSER_ROLE', async () => {
-      const { long, admin, pauser } = await loadFixture(fixture);
+      const { LONG, admin, pauser } = await loadFixture(fixture);
 
-      await expect(long.connect(admin).pause())
-        .to.be.revertedWithCustomError(long, 'AccessControlUnauthorizedAccount')
-        .withArgs(admin.address, await long.PAUSER_ROLE());
+      await expect(LONG.connect(admin).pause())
+        .to.be.revertedWithCustomError(LONG, 'AccessControlUnauthorizedAccount')
+        .withArgs(admin.address, await LONG.PAUSER_ROLE());
 
-      const pause = await long.connect(pauser).pause();
+      const pause = await LONG.connect(pauser).pause();
 
-      await expect(pause).to.emit(long, 'Paused').withArgs(pauser.address);
+      await expect(pause).to.emit(LONG, 'Paused').withArgs(pauser.address);
     });
 
     it('unpause() only with PAUSER_ROLE', async () => {
-      const { long, admin, pauser } = await loadFixture(fixture);
+      const { LONG, admin, pauser } = await loadFixture(fixture);
 
-      await long.connect(pauser).pause();
+      await LONG.connect(pauser).pause();
 
-      await expect(long.connect(admin).unpause())
-        .to.be.revertedWithCustomError(long, 'AccessControlUnauthorizedAccount')
-        .withArgs(admin.address, await long.PAUSER_ROLE());
+      await expect(LONG.connect(admin).unpause())
+        .to.be.revertedWithCustomError(LONG, 'AccessControlUnauthorizedAccount')
+        .withArgs(admin.address, await LONG.PAUSER_ROLE());
 
-      const unpause = await long.connect(pauser).unpause();
+      const unpause = await LONG.connect(pauser).unpause();
 
-      await expect(unpause).to.emit(long, 'Unpaused').withArgs(pauser.address);
+      await expect(unpause).to.emit(LONG, 'Unpaused').withArgs(pauser.address);
     });
   });
 });
