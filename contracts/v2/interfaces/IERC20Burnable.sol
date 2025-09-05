752da6dc39af050e186d446ce9cc9a6e5935e16d
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-05 16:03:33 +0300

    feat: add transfer for transferring to DEAD address

--- contracts/v2/interfaces/IERC20Burnable.sol
+++ contracts/v2/interfaces/IERC20Burnable.sol
@@ -3,6 +3,16 @@ pragma solidity 0.8.27;
 
 interface IERC20Burnable {
     /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must have a balance of at least `value`.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
      * @dev Destroys a `value` amount of tokens from the caller.
      *
      * See {ERC20-_burn}.
