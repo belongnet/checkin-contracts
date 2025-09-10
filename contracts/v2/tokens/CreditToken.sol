eab3694f881075625ef99dd0a0ef3b7d7d1a1ba7
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-10 14:41:14 +0300

    chore: lock pragma

--- contracts/v2/tokens/CreditToken.sol
+++ contracts/v2/tokens/CreditToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.27;
+pragma solidity 0.8.27;
 
 import {ERC1155Base} from "./base/ERC1155Base.sol";
 import {ERC1155Info} from "../Structures.sol";
