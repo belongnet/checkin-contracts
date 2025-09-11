962b1212dff17bebfe893d59b25dc0a86d1835ca
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-11 12:59:45 +0300

    feat: update price feed mocks

--- contracts/mocks/LONGPriceFeedMock.sol
+++ contracts/mocks/LONGPriceFeedMock.sol
@@ -20,7 +20,7 @@ contract LONGPriceFeedMockV1 {
     }
 
     /// @notice Returns 8 to emulate feed decimals.
-    function decimals() external pure returns (uint8) {
+    function decimals() external pure virtual returns (uint8) {
         return 8;
     }
 }
@@ -28,16 +28,38 @@ contract LONGPriceFeedMockV1 {
 /// @title LONGPriceFeedMockV2
 /// @notice Chainlink-like mock exposing v3-style `latestRoundData`.
 contract LONGPriceFeedMockV2 {
+    uint80 _roundId;
+    uint256 _updatedAt;
+    int256 _answer;
+
+    constructor(uint80 roundId, uint256 updatedAt, int256 answer) {
+        _roundId = roundId;
+        _updatedAt = updatedAt;
+        _answer = answer;
+    }
+
     /// @notice Returns a fixed price answer (8 decimals) with current timestamp.
     function latestRoundData()
         external
         view
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
-        return (0, 50000000, 0, block.timestamp, 0);
+        return (_roundId, _answer, 0, _updatedAt, 0);
+    }
+
+    /// @notice Returns 8 to emulate feed decimals.
+    function decimals() external pure virtual returns (uint8) {
+        return 8;
     }
 }
 
 /// @title LONGPriceFeedMockV3
 /// @notice Combined mock inheriting both v2 and v3 interfaces.
-contract LONGPriceFeedMockV3 is LONGPriceFeedMockV1, LONGPriceFeedMockV2 {}
+contract LONGPriceFeedMockV3 is LONGPriceFeedMockV1, LONGPriceFeedMockV2 {
+    constructor() LONGPriceFeedMockV2(252525, block.timestamp, 50000000) {}
+
+    /// @notice Returns 8 to emulate feed decimals.
+    function decimals() external pure override(LONGPriceFeedMockV1, LONGPriceFeedMockV2) returns (uint8) {
+        return 8;
+    }
+}
