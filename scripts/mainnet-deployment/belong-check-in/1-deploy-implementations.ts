2784734519101a5e240e179daffd0313a083028d
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-25 13:42:08 +0300

    fix: deploy impls script

--- scripts/mainnet-deployment/belong-check-in/1-deploy-implementations.ts
+++ scripts/mainnet-deployment/belong-check-in/1-deploy-implementations.ts
@@ -5,6 +5,7 @@ import {
   deployAccessTokenImplementation,
   deployCreditTokenImplementation,
   deployRoyaltiesReceiverV2Implementation,
+  deployVestingWalletImplementation,
 } from '../../../helpers/deployFixtures';
 import { ethers } from 'hardhat';
 
@@ -26,105 +27,110 @@ async function deploy() {
   }
 
   // Initialize deployments object
-  let deployments = {};
+  let deployments: any = {};
   if (fs.existsSync(deploymentFile)) {
     deployments = JSON.parse(fs.readFileSync(deploymentFile, 'utf-8'));
   }
 
+  if (!deployments.implementations) {
+    deployments.implementations = {};
+  }
+
   if (DEPLOY) {
-    console.log('Deploying AccessTokenImplementation: ');
-
-    const SignatureVerifier = deployments.SignatureVerifier.address;
-
+    console.log('Deploy Implementations: ');
     // Validate environment variables
-    if (!SignatureVerifier) {
+    if (!deployments.libraries.sigantureVerifier) {
       throw new Error('Missing required environment variables: SignatureVerifier');
     }
 
     // Validate addresses
-    for (const addr of [SignatureVerifier]) {
+    for (const addr of [deployments.libraries.sigantureVerifier]) {
       if (!ethers.utils.isAddress(addr)) {
         throw new Error(`Invalid address: ${addr}`);
       }
     }
 
-    const accessTokenImpl = await deployAccessTokenImplementation(SignatureVerifier);
+    console.log('Deploying AccessTokenImplementation contract...');
+    const accessTokenImpl = await deployAccessTokenImplementation(deployments.libraries.sigantureVerifier);
     // Update deployments object
-    deployments = {
-      ...deployments,
-      AccessTokenImplementation: {
-        address: accessTokenImpl.address,
-      },
-    };
-
+    deployments.implementations.accessToken = accessTokenImpl.address;
     // Write to file
     fs.writeFileSync(deploymentFile, JSON.stringify(deployments, null, 2));
     console.log('Deployed AccessTokenImplementation to: ', accessTokenImpl.address);
 
-    console.log('Deploying RoyaltiesReceiverV2Implementation: ');
+    console.log('Deploying CreditTokenImplementation contract...');
+    const creditTokenImpl = await deployCreditTokenImplementation();
+    // Update deployments object
+    deployments.implementations.creditToken = creditTokenImpl.address;
+    // Write to file
+    fs.writeFileSync(deploymentFile, JSON.stringify(deployments, null, 2));
+    console.log('Deployed CreditTokenImplementation to: ', creditTokenImpl.address);
+
+    console.log('Deploying RoyaltiesReceiverV2Implementation contract...');
     const rrImpl = await deployRoyaltiesReceiverV2Implementation();
     // Update deployments object
-    deployments = {
-      ...deployments,
-      RoyaltiesReceiverV2Implementation: {
-        address: rrImpl.address,
-      },
-    };
-
+    deployments.implementations.royaltiesReceiver = rrImpl.address;
     // Write to file
     fs.writeFileSync(deploymentFile, JSON.stringify(deployments, null, 2));
     console.log('Deployed RoyaltiesReceiverV2Implementation to: ', rrImpl.address);
 
-    console.log('Deploying CreditTokenImplementation: ');
-    const creditTokenImpl = await deployCreditTokenImplementation();
+    console.log('Deploying VestingWalletImplementation contract...');
+    const vestingWalletImpl = await deployVestingWalletImplementation();
     // Update deployments object
-    deployments = {
-      ...deployments,
-      CreditTokenImplementation: {
-        address: creditTokenImpl.address,
-      },
-    };
-
+    deployments.implementations.vestingWallet = vestingWalletImpl.address;
     // Write to file
     fs.writeFileSync(deploymentFile, JSON.stringify(deployments, null, 2));
-    console.log('Deployed CreditTokenImplementation to: ', creditTokenImpl.address);
+    console.log('Deployed VestingWalletImplementation to: ', vestingWalletImpl.address);
+
     console.log('Done.');
   }
 
   if (VERIFY) {
-    console.log('Verification AccessTokenImplementation:');
+    console.log('Verification AccessTokenImplementation: ');
     try {
-      if (!deployments.AccessTokenImplementation?.address) {
+      if (!deployments.implementations.accessToken) {
         throw new Error('No AccessTokenImplementation deployment data found for verification.');
       }
-      await verifyContract(deployments.AccessTokenImplementation.address);
+      await verifyContract(deployments.implementations.accessToken);
       console.log('AccessTokenImplementation verification successful.');
     } catch (error) {
-      console.error('AccessTokenImplementation verification failed:', error);
+      console.error('AccessTokenImplementation verification failed: ', error);
     }
     console.log('Done.');
 
-    console.log('Verification RoyaltiesReceiverV2Implementation:');
+    console.log('Verification CreditTokenImplementation: ');
     try {
-      if (!deployments.RoyaltiesReceiverV2Implementation?.address) {
-        throw new Error('No RoyaltiesReceiverV2Implementation deployment data found for verification.');
-      }
-      await verifyContract(deployments.RoyaltiesReceiverV2Implementation.address);
-      console.log('RoyaltiesReceiverV2Implementation verification successful.');
-    } catch (error) {
-      console.error('RoyaltiesReceiverV2Implementation verification failed:', error);
-    }
-    console.log('Done.');
-
-    console.log('Verification CreditTokenImplementation:');
-    try {
-      if (!deployments.CreditTokenImplementation?.address) {
+      if (!deployments.implementations.creditToken) {
         throw new Error('No CreditTokenImplementation deployment data found for verification.');
       }
-      await verifyContract(deployments.CreditTokenImplementation.address);
+      await verifyContract(deployments.implementations.creditToken);
       console.log('CreditTokenImplementation verification successful.');
     } catch (error) {
-      console.error('CreditTokenImplementation verification failed:', error);
+      console.error('CreditTokenImplementation verification failed: ', error);
+    }
+    console.log('Done.');
+
+    console.log('Verification RoyaltiesReceiverV2Implementation: ');
+    try {
+      if (!deployments.implementations.royaltiesReceiver) {
+        throw new Error('No RoyaltiesReceiverV2Implementation deployment data found for verification.');
+      }
+      await verifyContract(deployments.implementations.royaltiesReceiver);
+      console.log('RoyaltiesReceiverV2Implementation verification successful.');
+    } catch (error) {
+      console.error('RoyaltiesReceiverV2Implementation verification failed: ', error);
+    }
+    console.log('Done.');
+
+    console.log('Verification VestingWalletImplementation:');
+    try {
+      if (!deployments.implementations.vestingWallet) {
+        throw new Error('No VestingWalletImplementation deployment data found for verification.');
+      }
+      await verifyContract(deployments.implementations.vestingWallet);
+      console.log('VestingWalletImplementation verification successful.');
+    } catch (error) {
+      console.error('VestingWalletImplementation verification failed: ', error);
     }
     console.log('Done.');
   }
