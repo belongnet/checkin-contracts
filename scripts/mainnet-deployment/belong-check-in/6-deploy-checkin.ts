535c3e3c8cf3980d82fe0d909065ee21891dfb61
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-25 13:42:44 +0300

    fix: deploy checkin script

--- scripts/mainnet-deployment/belong-check-in/6-deploy-checkin.ts
+++ scripts/mainnet-deployment/belong-check-in/6-deploy-checkin.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import { verifyContract } from '../../../helpers/verify';
 import { ethers } from 'hardhat';
 import { deployBelongCheckIn } from '../../../helpers/deployFixtures';
+import { BigNumber } from 'ethers';
 
 dotenv.config();
 
@@ -23,45 +24,57 @@ async function deploy() {
   }
 
   // Initialize deployments object
-  let deployments = {};
+  let deployments: any = {};
   if (fs.existsSync(deploymentFile)) {
     deployments = JSON.parse(fs.readFileSync(deploymentFile, 'utf-8'));
   }
 
+  if (!deployments.checkIn) {
+    deployments.checkIn = {};
+  }
+
   if (DEPLOY) {
-    console.log('Deploying BelongCheckIn: ');
+    console.log('Deploy BelongCheckIn: ');
 
     // Read addresses from environment variables
 
     const owner = process.env.ADMIN_ADDRESS;
     const swapPoolFees = process.env.UNISWAPV3_POOL_FEES;
+    const swapV3Factory = process.env.UNISWAPV3_FACTORY_ADDRESS;
     const swapV3Router = process.env.UNISWAPV3_ROUTER_ADDRESS;
     const swapV3Quoter = process.env.UNISWAPV3_QUOTER_ADDRESS;
-    const weth = process.env.WETH_ADDRESS;
+    const wNativeCurrency = process.env.WNATIVE_ADDRESS;
     const usdc = process.env.USDC_ADDRESS;
-    const signatureVerifier = deployments.SignatureVerifier.address;
-    const helper = deployments.Helper.address;
-    const long = deployments.LONG.address;
 
     // Validate environment variables
     if (
-      !signatureVerifier ||
-      !helper ||
+      !deployments.libraries.sigantureVerifier ||
+      !deployments.libraries.helper ||
       !owner ||
       !swapPoolFees ||
+      !swapV3Factory ||
       !swapV3Router ||
       !swapV3Quoter ||
-      !weth ||
+      !wNativeCurrency ||
       !usdc ||
-      !long
+      !deployments.tokens.long
     ) {
       throw new Error(
-        'Missing required environment variables: SIGNATURE_VERIFIER_ADDRESS, HELPER_ADDRESS, OWNER_ADDRESS, UNISWAPV3_POOL_FEES, UNISWAPV3_ROUTER_ADDRESS, UNISWAPV3_QUOTER_ADDRESS, WETH_ADDRESS, USDC_ADDRESS, LONG_ADDRESS',
+        `Missing required environment variables:\nSignatureVerifier: ${deployments.libraries.sigantureVerifier}\nHELPER_ADDRESS: ${deployments.libraries.helper}\nOWNER_ADDRESS: ${owner}\nUNISWAPV3_POOL_FEES: ${swapPoolFees}\nUNISWAPV3_FACTORY_ADDRESS: ${swapV3Factory}\nUNISWAPV3_ROUTER_ADDRESS: ${swapV3Router}\nUNISWAPV3_QUOTER_ADDRESS: ${swapV3Quoter}\nWNATIVE_ADDRESS: ${wNativeCurrency}\nUSDC_ADDRESS: ${usdc}\nLong: ${deployments.tokens.long}`,
       );
     }
 
     // Validate addresses (exclude swapPoolFees as it's not an address)
-    for (const addr of [signatureVerifier, helper, owner, swapV3Router, swapV3Quoter, weth, usdc, long]) {
+    for (const addr of [
+      deployments.libraries.sigantureVerifier,
+      deployments.libraries.helper,
+      owner,
+      swapV3Router,
+      swapV3Quoter,
+      wNativeCurrency,
+      usdc,
+      deployments.tokens.long,
+    ]) {
       if (!ethers.utils.isAddress(addr)) {
         throw new Error(`Invalid address: ${addr}`);
       }
@@ -69,25 +82,28 @@ async function deploy() {
 
     // Construct paymentsInfo struct
     const paymentsInfo = {
+      slippageBps: BigNumber.from(10).pow(27).sub(1),
       swapPoolFees,
+      swapV3Factory,
       swapV3Router,
       swapV3Quoter,
-      weth,
+      wNativeCurrency,
       usdc,
-      long,
+      long: deployments.tokens.long,
+      maxPriceFeedDelay: 86_400,
     } as BelongCheckIn.PaymentsInfoStruct;
 
     console.log('Deploying BelongCheckIn contract...');
-    const belongCheckIn: BelongCheckIn = await deployBelongCheckIn(signatureVerifier, helper, owner, paymentsInfo);
+    const belongCheckIn: BelongCheckIn = await deployBelongCheckIn(
+      deployments.libraries.sigantureVerifier,
+      deployments.libraries.helper,
+      owner,
+      paymentsInfo,
+    );
 
     // Update deployments object
-    deployments = {
-      ...deployments,
-      BelongCheckIn: {
-        proxy: belongCheckIn.address,
-        parameters: [owner, paymentsInfo],
-      },
-    };
+    deployments.checkIn.address = belongCheckIn.address;
+    deployments.checkIn.paymentsInfo = paymentsInfo;
 
     // Write to file
     fs.writeFileSync(deploymentFile, JSON.stringify(deployments, null, 2));
@@ -98,10 +114,10 @@ async function deploy() {
   if (VERIFY) {
     console.log('Verification: ');
     try {
-      if (!deployments.BelongCheckIn?.address) {
+      if (!deployments.checkIn.address) {
         throw new Error('No BelongCheckIn deployment data found for verification.');
       }
-      await verifyContract(deployments.BelongCheckIn.address);
+      await verifyContract(deployments.checkIn.address);
       console.log('BelongCheckIn verification successful.');
     } catch (error) {
       console.error('BelongCheckIn verification failed: ', error);
