6e138f6141182f3642e3b62d6efcb85e1818d666
Bohdan Pukhno <pukhnobg@gmail.com>
2025-10-02 17:23:07 +0300

    feat: v3 router update

--- contracts/v2/interfaces/IV3Router.sol
+++ contracts/v2/interfaces/IV3Router.sol
@@ -10,7 +10,7 @@ interface IV3Router {
     /// @param deadline Unix timestamp after which the swap will revert.
     /// @param amountIn Exact amount of input tokens to swap.
     /// @param amountOutMinimum Minimum amount of output tokens expected (slippage protection).
-    struct ExactInputParams {
+    struct ExactInputParamsV1 {
         bytes path;
         address recipient;
         uint256 deadline;
@@ -18,8 +18,22 @@ interface IV3Router {
         uint256 amountOutMinimum;
     }
 
+    struct ExactInputParamsV2 {
+        bytes path;
+        address recipient;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+    }
+
     /// @notice Executes an exact-input swap along the provided path.
     /// @param params The exact-input swap parameters.
     /// @return amountOut The amount of output tokens received.
-    function exactInput(ExactInputParams calldata params) external payable returns (uint256 amountOut);
+    function exactInput(ExactInputParamsV1 calldata params) external payable returns (uint256 amountOut);
+
+    /// @notice Swaps `amountIn` of one token for as much as possible of another along the specified path
+    /// @dev Setting `amountIn` to 0 will cause the contract to look up its own balance,
+    /// and swap the entire amount, enabling contracts to send tokens before calling this function.
+    /// @param params The parameters necessary for the multi-hop swap, encoded as `ExactInputParams` in calldata
+    /// @return amountOut The amount of the received token
+    function exactInput(ExactInputParamsV2 calldata params) external payable returns (uint256 amountOut);
 }
