6ca6adafbd3f65e272d4e4a885e4d861c4bb3577
Bohdan Pukhno <pukhnobg@gmail.com>
2026-04-07 12:46:36 +0300

    feat: add verification script

---
+++ scripts/mainnet-deployment/belong-checkin/verify.ts
@@ -0,0 +1,14 @@
+import { verifyContract } from '../../../helpers/verify';
+
+const address = '0x3E02292e7A1Fe0EA5682BfADB1Cc7573bf28e4bA';
+async function verify() {
+  console.log('Verification: ');
+
+  try {
+    await verifyContract(address);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+verify();
